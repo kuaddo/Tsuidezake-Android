b2a4c28db0c8d00a8808ae1ffff53e24f8cfa3a9
shiita0903 <shiita0903@gmail.com>
2020-02-11 01:59:54 +0900

    Fix build settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,15 @@
+import de.mannodermaus.gradle.plugins.junit5.junitPlatform
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("de.mannodermaus.android-junit5")
+    id("deploygate")
 }
 
 android {
@@ -17,13 +23,58 @@ android {
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    signingConfigs {
+        getByName("debug") {
+            storeFile = rootProject.file("keystore/debug.keystore")
+            storePassword = Keystore.STORE_PASSWORD
+            keyAlias = Keystore.KEY_ALIAS
+            keyPassword = Keystore.KEY_PASSWORD
+        }
+    }
     buildTypes {
+        val appName = "ついで酒"
+        getByName("debug") {
+            resValue("string", "app_name", "$appName-debug")
+
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
         getByName("release") {
-            isMinifyEnabled = false
+            resValue("string", "app_name", appName)
+
+            isShrinkResources = true
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+    androidExtensions {
+        isExperimental = true
+    }
+    dataBinding {
+        isEnabled = true
+    }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    lintOptions {
+        disable("GoogleAppIndexingWarning")
+    }
+    testOptions {
+        junitPlatform {
+            filters {
+                includeEngines("spek2")
+            }
         }
     }
 }
