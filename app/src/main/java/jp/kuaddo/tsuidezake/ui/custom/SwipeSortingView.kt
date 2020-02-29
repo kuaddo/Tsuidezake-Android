1a4b61f663643413ea8137cded5fbf78ab264002
shiita0903 <shiita0903@gmail.com>
2020-02-29 19:38:06 +0900

    Remove buttons from swipe area.

--- app/src/main/java/jp/kuaddo/tsuidezake/ui/custom/SwipeSortingView.kt
+++ app/src/main/java/jp/kuaddo/tsuidezake/ui/custom/SwipeSortingView.kt
@@ -4,26 +4,34 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.google.android.material.chip.Chip
+import jp.kuaddo.tsuidezake.R
 import jp.kuaddo.tsuidezake.databinding.ViewRecommendDrinkBinding
 import jp.kuaddo.tsuidezake.model.DrinkDetail
-import java.util.*
+import java.util.Queue
 import java.util.concurrent.ArrayBlockingQueue
 
 class SwipeSortingView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : LinearLayout(context, attrs, defStyleAttr) {
 
     private val layoutInflater = LayoutInflater.from(context)
+    private val cardContainer: FrameLayout
     private val bindingQueue: Queue<ViewRecommendDrinkBinding> = ArrayBlockingQueue(2)
     private var nextDrinkIndex: Int = 0
 
     private var drinks: List<DrinkDetail>? = null
-    var onNextDrinkShown: ((DrinkDetail) -> Unit)? = null
     var onLastDrinkRemoved: (() -> Unit)? = null
+
+    init {
+        View.inflate(context, R.layout.view_swipe_sorting, this)
+        cardContainer = findViewById(R.id.card_container)
+    }
 
     fun submitDrinks(drinks: List<DrinkDetail>) {
         this.drinks = drinks
@@ -35,37 +43,7 @@ class SwipeSortingView @JvmOverloads constructor(
         nextDrinkIndex = minOf(2, drinks.size)
     }
 
-    private fun initializeRecommendDrinkBinding(drinkDetail: DrinkDetail) {
-        val binding = ViewRecommendDrinkBinding.inflate(layoutInflater).also {
-            it.drinkDetail = drinkDetail
-            it.tagsChipGroup.let { chipGroup ->
-                drinkDetail.tags.map { Chip(context).apply { text = it } }
-                    .forEach { chip -> chipGroup.addView(chip) }
-            }
-            it.executePendingBindings()
-        }
-        bindingQueue.offer(binding)
-        addView(binding.root, BACK)
-    }
-
-    private fun setOnTouchListener(index: Int) {
-        val targetView = getChildAt(index)
-        targetView.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_MOVE -> {
-                    v.x += event.x - v.width / 2
-                }
-                MotionEvent.ACTION_UP -> {
-                    // TODO: 要修正
-                    if (event.rawX.toInt() in (width / 3)..(width * 2 / 3)) v.x = 0f
-                    else replaceFront()
-                }
-            }
-            true
-        }
-    }
-
-    private fun replaceFront() {
+    fun replaceFront() {
         val drinks = drinks ?: return
         when {
             nextDrinkIndex > drinks.size -> {
@@ -88,14 +66,44 @@ class SwipeSortingView @JvmOverloads constructor(
             it.executePendingBindings()
         } ?: return
         bindingQueue.offer(binding)
-        addView(binding.root, BACK)
+        cardContainer.addView(binding.root, BACK)
+    }
+
+    private fun initializeRecommendDrinkBinding(drinkDetail: DrinkDetail) {
+        val binding = ViewRecommendDrinkBinding.inflate(layoutInflater).also {
+            it.drinkDetail = drinkDetail
+            it.tagsChipGroup.let { chipGroup ->
+                drinkDetail.tags.map { Chip(context).apply { text = it } }
+                    .forEach { chip -> chipGroup.addView(chip) }
+            }
+            it.executePendingBindings()
+        }
+        bindingQueue.offer(binding)
+        cardContainer.addView(binding.root, BACK)
+    }
+
+    private fun setOnTouchListener(index: Int) {
+        val targetView = cardContainer.getChildAt(index)
+        targetView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    v.x += event.x - v.width / 2
+                }
+                MotionEvent.ACTION_UP -> {
+                    // TODO: 要修正
+                    if (event.rawX.toInt() in (width / 3)..(width * 2 / 3)) v.x = 0f
+                    else replaceFront()
+                }
+            }
+            true
+        }
     }
 
     private fun resetViewState(index: Int) {
-        val targetView = getChildAt(index)
+        val targetView = cardContainer.getChildAt(index)
         targetView.x = 0f
         targetView.setOnTouchListener(null)
-        removeViewAt(index)
+        cardContainer.removeViewAt(index)
     }
 
     companion object {
