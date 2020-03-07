30e2cfddc0c7cee4c05b460d4c605dc7a6198444
shiita0903 <shiita0903@gmail.com>
2020-03-08 01:31:52 +0900

    Add dialog fragment.

--- app/src/main/java/jp/kuaddo/tsuidezake/ui/ranking/RankingFragment.kt
+++ app/src/main/java/jp/kuaddo/tsuidezake/ui/ranking/RankingFragment.kt
@@ -6,16 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
-import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materialdialogs.customview.customView
-import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
 import dagger.android.support.DaggerFragment
 import jp.kuaddo.tsuidezake.R
 import jp.kuaddo.tsuidezake.databinding.FragmentRankingBinding
 import jp.kuaddo.tsuidezake.extensions.dataBinding
 import jp.kuaddo.tsuidezake.model.Drink
 import jp.kuaddo.tsuidezake.model.DrinkDetail
-import jp.kuaddo.tsuidezake.ui.custom.SwipeSortingView
 
 class RankingFragment : DaggerFragment() {
     private val binding by dataBinding<FragmentRankingBinding>(R.layout.fragment_ranking)
@@ -56,27 +52,6 @@ class RankingFragment : DaggerFragment() {
         showRecommendDrinkDialog()
     }
 
-    private fun showRecommendDrinkDialog() {
-        context?.let {
-            var dialog: MaterialDialog? = null
-            val swipeSortingView = SwipeSortingView(it).apply {
-                val drinks = (1..5).map { index ->
-                    DrinkDetail(
-                        Drink(index, "獺祭", "日本酒"),
-                        10000 * index,
-                        "",
-                        tags = listOf("辛口", "初心者におすすめ", "コスパ良し")
-                    )
-                }
-                submitDrinks(drinks)
-                onLastDrinkRemoved = { dialog?.dismiss() }
-            }
-            dialog = MaterialDialog(it).show {
-                cornerRadius(res = R.dimen.material_dialog_corner_radius)
-                lifecycleOwner(viewLifecycleOwner)
-                customView(view = swipeSortingView, noVerticalPadding = true)
-                cancelOnTouchOutside(false)
-            }
-        }
-    }
+    private fun showRecommendDrinkDialog() =
+        findNavController().navigate(RankingFragmentDirections.actionRankingToSwipeSortingDialog())
 }
---
+++ app/src/main/java/jp/kuaddo/tsuidezake/ui/ranking/SwipeSortingDialogFragment.kt
@@ -0,0 +1,45 @@
+package jp.kuaddo.tsuidezake.ui.ranking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import jp.kuaddo.tsuidezake.R
+import jp.kuaddo.tsuidezake.databinding.DialogSwipeSortingBinding
+import jp.kuaddo.tsuidezake.extensions.dataBinding
+import jp.kuaddo.tsuidezake.model.Drink
+import jp.kuaddo.tsuidezake.model.DrinkDetail
+
+class SwipeSortingDialogFragment : DialogFragment() {
+
+    private val binding by dataBinding<DialogSwipeSortingBinding>(R.layout.dialog_swipe_sorting)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.AppTheme_FullScreenDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.swipeSortingView.apply {
+            val drinks = (1..5).map { index ->
+                DrinkDetail(
+                    Drink(index, "獺祭", "日本酒"),
+                    10000 * index,
+                    "",
+                    tags = listOf("辛口", "初心者におすすめ", "コスパ良し")
+                )
+            }
+            submitDrinks(drinks)
+            onLastDrinkRemoved = { dialog?.dismiss() }
+        }
+    }
+}
