63b638bd4e4f0c20bc4142351c7a34937c8705f6
shiita0903 <shiita0903@gmail.com>
2020-05-28 01:37:15 +0900

    Add jacoco and Codecov upload.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
     id("de.mannodermaus.android-junit5")
     id("com.github.ben-manes.versions")
+    id("jacoco")
     id("deploygate")
 }
 
@@ -56,6 +57,7 @@ android {
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
             isDebuggable = true
+            isTestCoverageEnabled = true
             signingConfig = signingConfigs.getByName("debug")
         }
         getByName("release") {
@@ -87,6 +89,13 @@ android {
         disable("GoogleAppIndexingWarning")
     }
     testOptions {
+        unitTests.apply {
+            all {
+                extensions.configure(JacocoTaskExtension::class.java) {
+                    isIncludeNoLocationClasses = true
+                }
+            }
+        }
         junitPlatform {
             filters {
                 includeEngines("spek2")
@@ -205,9 +214,37 @@ tasks.withType<DependencyUpdatesTask> {
     }
 }
 
+task("jacocoTestReport", JacocoReport::class) {
+    dependsOn("testDebugUnitTest")
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+        csv.isEnabled = false
+    }
+    sourceDirectories.setFrom("${projectDir}/src/main/java")
+    classDirectories.setFrom(fileTree(
+        "dir" to ".",
+        "includes" to listOf("**/tmp/kotlin-classes/debug/**"),
+        "excludes" to listOf(
+            // TODO: うまく動いてないっぽいので修正する
+            // Android
+            "**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*", "**/*Test*.*",
+            "**/*Spec*.*", "android/**/*.*", "**/*Application.*",
+
+            // Dagger
+            "**/*Dagger*Component*.*", "**/*Module.*", "**/*Module$*.*", "**/*MembersInjector*.*",
+            "**/*_Factory*.*", "**/*Provide*Factory*.*"
+        )
+    ))
+    executionData.setFrom(files("${buildDir}/jacoco/testDebugUnitTest.exec"))
+}
+
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
 }
+
+fun com.android.build.gradle.internal.dsl.TestOptions.UnitTestOptions.all(block: Test.() -> Unit) =
+    all(KotlinClosure1<Any, Test>({ (this as Test).apply(block) }, owner = this))
