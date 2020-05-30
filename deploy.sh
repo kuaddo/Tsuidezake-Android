ff7c4594841380c150d0cbd5b502134491198908
shiita0903 <shiita0903@gmail.com>
2020-05-30 15:03:33 +0900

    Extract deploy script.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 一旦fetchしないと対象のコミットを見ることができない
+git fetch --prune --unshallow
+COMMIT_MESSAGE=`git log --pretty=format:%s -n1 ${{ github.event.after }}`
+
+if [[ $COMMIT_MESSAGE == *\[Deploy\]* || $COMMIT_MESSAGE == *\[deploy\]* ]]; then
+  curl \
+    -F "token=${{secrets.DEPLOY_GATE_TOKEN}}" \
+    -F "file=@app/build/outputs/apk/debug/app-debug.apk" \
+    -F "message=${COMMIT_MESSAGE}" \
+    https://deploygate.com/api/users/${{secrets.DEPLOY_GATE_USER_NAME}}/apps;
+else
+  echo "Did not upload APK to DeployGate";
+fi
