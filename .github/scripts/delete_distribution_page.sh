31297b6da6f6fdfcd708218b504059bab8921040
shiita0903 <shiita0903@gmail.com>
2020-05-30 22:17:24 +0900

    Fix package name.

--- .github/scripts/delete_distribution_page.sh
+++ .github/scripts/delete_distribution_page.sh
@@ -4,4 +4,4 @@ curl \
  -X DELETE \
  -F "token=$DEPLOY_GATE_TOKEN" \
  -F "distribution_name=$GITHUB_HEAD_REF" \
- https://deploygate.com/api/users/$DEPLOY_GATE_USER_NAME/platforms/android/apps/jp.kuaddo.tsuidezake/distributions
+ https://deploygate.com/api/users/$DEPLOY_GATE_USER_NAME/platforms/android/apps/jp.kuaddo.tsuidezake.debug/distributions
