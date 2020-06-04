980b7cdc90586772e307f6e3ec522b70c5deb31f
shiita0903 <shiita0903@gmail.com>
2020-06-05 00:36:18 +0900

    Add data loading to DrinkDetailViewModel.kt.

--- app/src/main/java/jp/kuaddo/tsuidezake/ui/drink/DrinkDetailViewModel.kt
+++ app/src/main/java/jp/kuaddo/tsuidezake/ui/drink/DrinkDetailViewModel.kt
@@ -3,9 +3,30 @@ package jp.kuaddo.tsuidezake.ui.drink
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import javax.inject.Inject
+import androidx.lifecycle.liveData
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import jp.kuaddo.tsuidezake.data.Repository
+import jp.kuaddo.tsuidezake.delegate.SnackbarViewModelDelegate
+import jp.kuaddo.tsuidezake.model.ErrorResource
+import jp.kuaddo.tsuidezake.model.SakeDetail
+import jp.kuaddo.tsuidezake.model.SuccessResource
+import jp.kuaddo.tsuidezake.util.SnackbarMessageText
 
-class DrinkDetailViewModel @Inject constructor() : ViewModel() {
+class DrinkDetailViewModel @AssistedInject constructor(
+    @Assisted private val sakeId: Int,
+    private val repository: Repository,
+    snackbarViewModelDelegate: SnackbarViewModelDelegate
+) : ViewModel(),
+    SnackbarViewModelDelegate by snackbarViewModelDelegate {
+
+    val sakeDetail: LiveData<SakeDetail> = liveData {
+        when (val res = repository.getSakeDetail(sakeId)) {
+            is SuccessResource -> emit(res.data)
+            is ErrorResource -> setMessage(SnackbarMessageText(res.message))
+        }
+    }
+
     // TODO: この部分はCustomViewに責務を分けたい
     val isExpanded: LiveData<Boolean>
         get() = _isExpanded
@@ -14,5 +35,10 @@ class DrinkDetailViewModel @Inject constructor() : ViewModel() {
 
     fun switchExpandState() {
         _isExpanded.value = _isExpanded.value?.not() ?: true
+    }
+
+    @AssistedInject.Factory
+    interface Factory {
+        fun create(sakeId: Int): DrinkDetailViewModel
     }
 }
