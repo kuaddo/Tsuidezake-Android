3839691aab8f73a18b995b228963755fdc4676f7
shiita0903 <shiita0903@gmail.com>
2020-08-05 01:39:59 +0900

    Apply lint.

--- app/src/main/java/jp/kuaddo/tsuidezake/bindingadapters/ViewBindingAdapters.kt
+++ app/src/main/java/jp/kuaddo/tsuidezake/bindingadapters/ViewBindingAdapters.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup.MarginLayoutParams
 import androidx.databinding.BindingAdapter
 import kotlin.math.roundToInt
 
-
 @BindingAdapter("visibleGone")
 fun View.bindVisibilityGone(isVisible: Boolean) {
     visibility = if (isVisible) View.VISIBLE else View.GONE
