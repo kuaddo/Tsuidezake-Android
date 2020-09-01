43ef4d7500239d50406ce1969a8344746bc95541
shiita0903 <shiita0903@gmail.com>
2020-09-01 23:53:03 +0900

    Add getRankings() to Repository.

--- data/repository/src/main/java/jp/kuaddo/tsuidezake/data/repository/Repository.kt
+++ data/repository/src/main/java/jp/kuaddo/tsuidezake/data/repository/Repository.kt
@@ -1,8 +1,10 @@
 package jp.kuaddo.tsuidezake.data.repository
 
+import jp.kuaddo.tsuidezake.model.Ranking
 import jp.kuaddo.tsuidezake.model.Resource
 import jp.kuaddo.tsuidezake.model.SakeDetail
 
 interface Repository {
+    suspend fun getRankings(): Resource<List<Ranking>>
     suspend fun getSakeDetail(id: Int): Resource<SakeDetail>
 }
