c5a86d6f12fdc4abf38f6f48e8ca9cfd123564d3
shiita0903 <shiita0903@gmail.com>
2020-11-26 02:22:27 +0900

    Replace ::set-env with $GITHUB_ENV.

--- .github/scripts/deploy_on_pr.sh
+++ .github/scripts/deploy_on_pr.sh
@@ -3,7 +3,7 @@
 # 一旦fetchしないと対象のコミットを見ることができない
 git fetch --prune --unshallow
 COMMIT_MESSAGE=`git log --pretty=format:%s -n2 | tail -n1`
-echo "::set-env name=DEPLOY_MESSAGE::$COMMIT_MESSAGE"
+echo "DEPLOY_MESSAGE = $COMMIT_MESSAGE" >> $GITHUB_ENV
 echo "commit message = $COMMIT_MESSAGE"
 
 if [[ $COMMIT_MESSAGE == *\[Deploy\]* || $COMMIT_MESSAGE == *\[deploy\]* ]]; then
@@ -14,9 +14,9 @@ if [[ $COMMIT_MESSAGE == *\[Deploy\]* || $COMMIT_MESSAGE == *\[deploy\]* ]]; the
     https://deploygate.com/api/users/$DEPLOY_GATE_USER_NAME/apps`
   ERROR=`echo $RESULT | jq .error`
   if [[ $ERROR == true ]]; then DEPLOY_SUCCESS=false; else DEPLOY_SUCCESS=true; fi
-  echo "::set-env name=DEPLOY_SUCCESS::$DEPLOY_SUCCESS"
+  echo "DEPLOY_SUCCESS = $DEPLOY_SUCCESS" >> $GITHUB_ENV
   if [[ $ERROR == true ]]; then echo $RESULT; fi
 else
-  echo "::set-env name=DEPLOY_SUCCESS::false"
+  echo "DEPLOY_SUCCESS = false" >> $GITHUB_ENV
   echo "Did not upload APK to DeployGate"
 fi
