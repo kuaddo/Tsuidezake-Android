ab049345fc038c44860dcdca15b2c6ab5a1ee13b
shiita0903 <shiita0903@gmail.com>
2021-11-16 01:59:50 +0900

    Add gradle information to merged_report.

--- buildSrc/src/main/java/DependencyUpdatesReportMergerTask.kt
+++ buildSrc/src/main/java/DependencyUpdatesReportMergerTask.kt
@@ -1,11 +1,11 @@
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.io.FileOutputStream
 import java.nio.file.Paths
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
 
 open class DependencyUpdatesReportMergerTask : DefaultTask() {
     @get:Input
@@ -13,15 +13,29 @@ open class DependencyUpdatesReportMergerTask : DefaultTask() {
 
     @TaskAction
     fun createMergedReport() {
-        val gson = Gson()
-        val dependencyMap = File(projectRootDirPath).walk()
+        val dependencyMap = getDependencyMap().toMutableMap()
+        getGradleDependency()?.let { dependencyMap.put("Gradle", listOf(it)) }
+
+        val outputFile = Paths.get(
+            projectRootDirPath,
+            "app",
+            "build",
+            "dependencyUpdates",
+            "merged_report.md"
+        ).toFile()
+        outputFile.delete()
+        writeTo(outputFile, dependencyMap)
+    }
+
+    private fun getDependencyMap(): Map<String, List<DependencyOutputItem>> =
+        File(projectRootDirPath).walk()
             .filter { it.endsWith(File("dependencyUpdates", "report.json")) }
             .flatMap { path ->
                 val moduleName = getModuleName(path.toString())
-                gson.fromJson(path.readText(), JsonObject::class.java)
+                GSON.fromJson(path.readText(), JsonObject::class.java)
                     .getAsJsonObject("outdated")
                     .getAsJsonArray("dependencies")
-                    .map { gson.fromJson(it, Dependency::class.java) to moduleName }
+                    .map { GSON.fromJson(it, Dependency::class.java) to moduleName }
                     .asSequence()
             }
             .groupBy(
@@ -37,15 +51,27 @@ open class DependencyUpdatesReportMergerTask : DefaultTask() {
                 valueTransform = { (key, value) -> DependencyOutputItem.from(key, value) }
             )
 
-        val outputFile = Paths.get(
-            projectRootDirPath,
-            "app",
-            "build",
-            "dependencyUpdates",
-            "merged_report.md"
-        ).toFile()
-        outputFile.delete()
-        writeTo(outputFile, dependencyMap)
+    private fun getGradleDependency(): DependencyOutputItem? {
+        val appReportFile = File(projectRootDirPath, "app/build/dependencyUpdates/report.json")
+        if (!appReportFile.exists() || !appReportFile.isFile) {
+            return null
+        }
+
+        val gradleJsonObject = GSON.fromJson(appReportFile.readText(), JsonObject::class.java)
+            .getAsJsonObject("gradle")
+        val runningVersion = gradleJsonObject
+            .getAsJsonObject("running")
+            .getAsJsonPrimitive("version")
+            .asString
+        val currentVersion = gradleJsonObject
+            .getAsJsonObject("current")
+            .getAsJsonPrimitive("version")
+            .asString
+        return if (runningVersion == currentVersion) {
+            null
+        } else {
+            DependencyOutputItem.fromForGradle(runningVersion, currentVersion)
+        }
     }
 
     private fun writeTo(outputFile: File, dependencyMap: Map<String, List<DependencyOutputItem>>) {
@@ -69,6 +95,10 @@ open class DependencyUpdatesReportMergerTask : DefaultTask() {
     private fun getModuleName(path: String) = path
         .substringAfter(projectRootDirPath + File.separator)
         .substringBefore("${File.separator}build${File.separator}")
+
+    companion object {
+        private val GSON = Gson()
+    }
 }
 
 data class Dependency(
@@ -90,13 +120,20 @@ data class DependencyOutputItem(
     val versionInfo: String,
     val moduleInfo: String
 ) {
-    override fun toString(): String = "$name [$versionInfo] ($moduleInfo)"
+    override fun toString(): String =
+        "$name [$versionInfo]" + if (moduleInfo.isEmpty()) "" else " ($moduleInfo)"
 
     companion object {
         fun from(dependency: Dependency, modules: List<String>) = DependencyOutputItem(
             name = dependency.name,
             versionInfo = "${dependency.version} -> ${dependency.available.milestone}",
             moduleInfo = modules.joinToString(separator = ", ")
+        )
+
+        fun fromForGradle(runningVersion: String, currentVersion: String) = DependencyOutputItem(
+            name = "Gradle",
+            versionInfo = "$runningVersion -> $currentVersion",
+            moduleInfo = ""
         )
     }
 }
